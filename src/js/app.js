6cd0a33aedae73cd407d75f8e5321e82948f6b9e
MYX <it.anton22@gmail.com>
2023-11-28 12:05:16 +0300

    completed task

--- src/js/app.js
+++ src/js/app.js
@@ -1,13 +1,20 @@
 
 export default class Validator {
-    constructor(userName) {
-      this.userName = userName;
-    }
-  
-    validateUsername() {
-      const regex = /^[a-z](?:(?!\d{3,})[\w-])*[a-z]$/i;
-  
-      return regex.test(this.userName);
+
+      static validatePhone(phoneNumber) {
+
+          const clearNumber = phoneNumber.replace(/\D/g, '');
+          if(clearNumber.length === 11 && clearNumber.startsWith('8')) {
+            return '+7' + clearNumber.slice(1);
+          } else if (clearNumber.length === 12) {
+            return `+${clearNumber}`;
+          } else if (clearNumber.length < 11) {
+            return 'Phone number error!';
+          } 
+
+          return `+${clearNumber}`;
+          
+       
     }
   }
   
