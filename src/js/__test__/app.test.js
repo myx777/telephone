f151405e58b01ff237049757e271d1904c4f479e
MYX <it.anton22@gmail.com>
2023-11-28 12:10:03 +0300

    correct with eslint

--- src/js/__test__/app.test.js
+++ src/js/__test__/app.test.js
@@ -3,16 +3,16 @@ import Validator from '../app';
 
 describe('Validator', () => {
   const testCases = [
-    { phoneNumber: '+79270000000', expectedResult: '+79270000000' },// РФ
+    { phoneNumber: '+79270000000', expectedResult: '+79270000000' }, // РФ
     { phoneNumber: '89270000000', expectedResult: '+79270000000' },
     { phoneNumber: '+7 927 000 00 00', expectedResult: '+79270000000' },
-    { phoneNumber: '+7 (927) 000-00-00', expectedResult:'+79270000000' },
-    { phoneNumber: 'phoneNumber', expectedResult: `Phone number error!` },
-    { phoneNumber: '+7 phoneNumber 1', expectedResult: `Phone number error!` },
-    { phoneNumber: '+860000000000', expectedResult: '+860000000000' },// Китай
+    { phoneNumber: '+7 (927) 000-00-00', expectedResult: '+79270000000' },
+    { phoneNumber: 'phoneNumber', expectedResult: 'Phone number error!' },
+    { phoneNumber: '+7 phoneNumber 1', expectedResult: 'Phone number error!' },
+    { phoneNumber: '+860000000000', expectedResult: '+860000000000' }, // Китай
     { phoneNumber: '+86 000 000 0000', expectedResult: '+860000000000' },
-    { phoneNumber: '+1 (555) 123-4567', expectedResult: '+15551234567' },// США
-    { phoneNumber: '+44 20 7946 0958', expectedResult: '+442079460958' },// Великобритания
+    { phoneNumber: '+1 (555) 123-4567', expectedResult: '+15551234567' }, // США
+    { phoneNumber: '+44 20 7946 0958', expectedResult: '+442079460958' }, // Великобритания
     // можно добавить еще тестовые случаи, при необходимости
   ];
 
--- src/js/app.js
+++ src/js/app.js
@@ -1,20 +1,14 @@
-
 export default class Validator {
-
-      static validatePhone(phoneNumber) {
-
-          const clearNumber = phoneNumber.replace(/\D/g, '');
-          if(clearNumber.length === 11 && clearNumber.startsWith('8')) {
-            return '+7' + clearNumber.slice(1);
-          } else if (clearNumber.length === 12) {
-            return `+${clearNumber}`;
-          } else if (clearNumber.length < 11) {
-            return 'Phone number error!';
-          } 
-
-          return `+${clearNumber}`;
-          
-       
+  static validatePhone(phoneNumber) {
+    const clearNumber = phoneNumber.replace(/\D/g, '');
+    if (clearNumber.length === 11 && clearNumber.startsWith('8')) {
+      return `+7${clearNumber.slice(1)}`;
+    } if (clearNumber.length === 12) {
+      return `+${clearNumber}`;
+    } if (clearNumber.length < 11) {
+      return 'Phone number error!';
     }
+
+    return `+${clearNumber}`;
   }
-  
+}
